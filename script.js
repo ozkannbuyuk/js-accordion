440769901f6ca8d4ef9ceea3faacc7803ef93cd1
ozkanbuyuk <ozkanbuyuk@icloud.com>
2023-12-25 22:12:18 +0300

    JavaScript - Accordion

---
+++ script.js
@@ -0,0 +1,13 @@
+let acc = document.querySelectorAll("#accordion .accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
